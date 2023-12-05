fca22ef5ac33bca5074eba9b1ffb971754379d51
Lewis Bamford <lewis.j.bamford@gmail.com>
2023-12-05 12:00:45 +1100

    Day 1 - Part 2 attempt

--- Puzzle1/main.go
+++ Puzzle1/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"bufio"
+	"strings"
 )
 
 func check(e error){
@@ -21,24 +22,43 @@ func main() {
 	total := 0
 	zeroRune := rune('0')
 	nineRune := rune('9')
+	digitStrs := [9]string{"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"}
 	for scanner.Scan() {
 		line := scanner.Text()
+		firstStrDigit := -1
+		firstDigitVal := 0
+		lastStrDigit := -1
+		lastDigitVal := 0
+		for i, s := range digitStrs {
+			index := strings.Index(line, s)
+			if index != -1 && (index < firstStrDigit || firstStrDigit == -1) {
+				firstStrDigit = index
+				firstDigitVal = i + 1
+			}
+
+			lastIndex := strings.LastIndex(line, s)
+			if index != -1 && (index > lastStrDigit || lastStrDigit == -1) {
+				lastStrDigit = lastIndex
+				lastDigitVal = i + 1
+			}
+		}
+
 		runes := []rune(line)
-		lineNumber := 0
 		for i := 0; i < len(runes); i++ {
 			char := runes[i]
-			if char > zeroRune && char <= nineRune {
-				lineNumber += int(char - '0') * 10
+			if char > zeroRune && char <= nineRune && (firstStrDigit == -1 || (firstStrDigit != -1 && i < firstStrDigit)) { 
+				firstDigitVal = int(char - '0')
 				break
 			}
 		}
 		for i := len(runes)-1; i >= 0; i-- {
 			char := runes[i]
-			if char > zeroRune && char <= nineRune {
-				lineNumber += int(char - '0') 
+			if char > zeroRune && char <= nineRune && (lastStrDigit == -1 || (lastStrDigit != -1 && i > lastStrDigit)) {
+				lastDigitVal = int(char - '0') 
 				break
 			}
 		}
+		lineNumber := firstDigitVal * 10 + lastDigitVal
 		fmt.Println(lineNumber)
 		total += lineNumber
 	}
